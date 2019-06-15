c3174345fc05dea81c45228ffdfcfefa318e860e
SonnySkyez <hnic85@gmail.com>
2019-06-15 13:43:20 +0200

    Fix debugging problem - images shows up

--- movierater/settings.py
+++ movierater/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['movie-rater-app.herokuapp.com', 'localhost:8000', '127.0.0.1']
 
