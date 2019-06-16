9252832418f8d3ca570885531dacd8902279c246
SonnySkyez <hnic85@gmail.com>
2019-06-16 23:06:51 +0200

    Edit def in models

--- main/models.py
+++ main/models.py
@@ -6,11 +6,15 @@ class Movie(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(default='', blank=True, null=True)
     year = models.CharField(null=True, blank=True, max_length=4)
-    photo = models.FileField(null=True, blank=True, default='blank.jpg')
+    photo = models.ImageField(null=True, blank=True, default='blank.jpg')
 
 
     def __str__(self):
         return self.name_with_year()
 
+
     def name_with_year(self):
-        return str(self.name) + ' (' + str(self.year) + ')'
+        if self.year:
+            return str(self.name) + ' (' + str(self.year) + ')'
+        else:
+            return str(self.name)
