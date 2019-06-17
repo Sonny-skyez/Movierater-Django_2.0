610488ab07b8502c67d819fc59de257102582691
SonnySkyez <hnic85@gmail.com>
2019-06-17 15:58:05 +0200

    add multichoice field and model

--- main/forms.py
+++ main/forms.py
@@ -5,5 +5,5 @@ class MovieForm(ModelForm):
 
     class Meta:
         model = Movie
-        fields = ['name', 'description', 'year', 'photo']     # lub exclude - te których ma nie być w projekcie.
+        fields = ['name', 'reccomendation', 'description', 'year', 'photo']     # lub exclude - te których ma nie być w projekcie.
 
--- main/models.py
+++ main/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from multiselectfield import MultiSelectField
+
+
+CHOICES = (('choice_1', 'Polecam!'),
+           ('choice_2', 'Nie polecam')
+           )
+
 
 
 class Movie(models.Model):
 
     name = models.CharField(max_length=128)
+    reccomendation = MultiSelectField(choices=CHOICES, max_choices=1, null=True, blank=True)
     description = models.TextField(default='', blank=True, null=True)
     year = models.CharField(null=True, blank=True, max_length=4)
     photo = models.ImageField(null=True, blank=True, default='blank.jpg')
